19a4a398a3c5c93d7c2860270e4981e1de1c1963
Harshit Rastogi <rastogiharshit14@gmail.com>
2021-02-03 16:45:32 +0530

    main

---
+++ CHFTIRED.cpp
@@ -0,0 +1,20 @@
+//Program by Harshit Rastogi
+#include <bits/stdc++.h> 
+#define pb push_back
+using namespace std;
+string abc()
+{
+    long long a,b;
+    cin>>a>>b;
+    return "YES";
+}
+int main()
+{
+int t ;
+cin>>t;
+ while (t--)
+{
+cout<<abc()<<endl;
+}
+return 0;
+}
